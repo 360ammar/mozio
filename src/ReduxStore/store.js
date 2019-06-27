1cc6e342da2f37a1cb1170d832071ea2d9bc6ec2
Ammar Rashad <ammar.rashad1@gmail.com>
2019-06-27 16:44:33 +0500

    Update store.js

--- src/ReduxStore/store.js
+++ src/ReduxStore/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import travelData from './reducers'
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store: Function = createStore(
   travelData,
-  composeWithDevTools(applyMiddleware(thunk))
+  compose(applyMiddleware(thunk))
 )
 
 export default store
